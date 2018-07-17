d46f44e5224e4c776ebcc63f140323c833c78ebf
said <alsaghir.said@gmail.com>
2018-07-17 11:15:59 +0200

    finished the first test...

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
 (function(){
 
-})
+ class DogApi {
+
+   constructor(){
+     this.baseUrl = 'https://dog.ceo/api/';
+   }
+
+ }
+
+const api = new DogApi();
+// First test
+// console.log(api.baseUrl) // The base url
+
+// // Second test
+
+// api.getBreeds().then(res => console.log(res));
+
+// // Third test
+// api.getBreedImage('african').then(res => console.log(res));
+})();
